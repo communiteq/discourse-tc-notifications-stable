f44bf09832e1b20c77e77043f4be53d9edfda9d4
Richard <richard@communiteq.com>
2023-04-18 11:16:41 +0000

    Working version

--- javascripts/discourse/initializers/notifications-stable.js
+++ javascripts/discourse/initializers/notifications-stable.js
@@ -1,5 +1,7 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { helperContext } from "discourse-common/lib/helpers";
+import { ajax } from "discourse/lib/ajax";
+import KeyValueStore from "discourse/lib/key-value-store";
 
 export default {
   name: "discourse-notifications-stable",
@@ -20,10 +22,11 @@ export default {
                 navigator.serviceWorker.controller.state === "activated"
               )
             ) {
-              return false;
+                return false;
             }
             return true;
         }
+
         api.modifyClass("component:desktop-notification-config", {
           isPushNotificationsPreferred() {
             return (
@@ -32,6 +35,89 @@ export default {
                 myPushNotificationsSupported()
             );
           },
+
+          actions: {
+            turnoff() {
+                function myUnsubscribePushNotification(user, callback) {
+                    const keyValueStore = new KeyValueStore("discourse_push_notifications_");
+                    keyValueStore.setItem(userSubscriptionKey(user), "");
+                    navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+                      serviceWorkerRegistration.pushManager
+                        .getSubscription()
+                        .then((subscription) => {
+                          if (subscription) {
+                            subscription.unsubscribe().then((successful) => {
+                              if (successful) {
+                                ajax("/push_notifications/unsubscribe", {
+                                  type: "POST",
+                                  data: { subscription: subscription.toJSON() },
+                                });
+                              }
+                            });
+                          }
+                        })
+                        .catch((e) => {
+                          // eslint-disable-next-line no-console
+                          console.error(e);
+                        });
+                      if (callback) {
+                        callback();
+                      }
+                    });
+                }
+                if (this.isEnabledDesktop) {
+                    this.set("notificationsDisabled", "disabled");
+                    this.notifyPropertyChange("notificationsPermission");
+                }
+                if (this.isEnabledPush) {
+                    myUnsubscribePushNotification(this.currentUser, () => {
+                      this.set("isEnabledPush", "");
+                    });
+                }
+            },
+            turnon() {
+                function mySubscribePushNotification(callback, applicationServerKey) {
+                    navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+                      serviceWorkerRegistration.pushManager
+                        .subscribe({
+                          userVisibleOnly: true,
+                          applicationServerKey: new Uint8Array(applicationServerKey.split("|")), // eslint-disable-line no-undef
+                        })
+                        .then((subscription) => {
+                          sendSubscriptionToServer(subscription, true);
+                          if (callback) {
+                            callback();
+                          }
+                        })
+                        .catch((e) => {
+                          // eslint-disable-next-line no-console
+                          console.error(e);
+                        });
+                    });
+                }
+                function sendSubscriptionToServer(subscription, sendConfirmation) {
+                    ajax("/push_notifications/subscribe", {
+                        type: "POST",
+                        data: {
+                        subscription: subscription.toJSON(),
+                        send_confirmation: sendConfirmation,
+                        },
+                    });
+                }
+
+                if (this.isPushNotificationsPreferred()) {
+                  mySubscribePushNotification(() => {
+                    this.set("isEnabledPush", "subscribed");
+                  }, this.siteSettings.vapid_public_key_bytes);
+                } else {
+                  this.set("notificationsDisabled", "");
+                  Notification.requestPermission(() => {
+                    confirmNotification(this.siteSettings);
+                    this.notifyPropertyChange("notificationsPermission");
+                  });
+                }
+              },
+            }
         });
     });
   }
